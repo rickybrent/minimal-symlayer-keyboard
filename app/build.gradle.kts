595b462e9e90cad99b4c728e139b1dbf1d2e78b7
Ricky Brent <ricky@rickybrent.com>
2025-08-24 01:56:18 -0700

    Remove experimental option.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 		minSdk = 29
 		targetSdk = 33
 		versionCode = 1
-		versionName = "0.5"
+		versionName = "0.76"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
